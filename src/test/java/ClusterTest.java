a6d3a9be454fd2a8f4a0b9c26de39aa70bc742e3
csq <402329396@qq.com>
2022-10-17 16:47:51 +0800

    .

--- src/test/java/ClusterTest.java
+++ src/test/java/ClusterTest.java
@@ -1,6 +1,16 @@
-public class ClusterTest {
+import org.junit.Test;
 
+public class ClusterTest {
+    @Test
     public void bankTest() throws Exception {
-        new Cluster().run(1,"");
+        new Cluster().run(6,"src/main/resources/bank_data.arff");
+    }
+    @Test
+    public void bmwTest() throws Exception{
+        new Cluster().run(5,"src/main/resources/bmw-browsers.arff");
+    }
+    @Test
+    public void irisTest() throws Exception{
+        new Cluster().run(3,"src/main/resources/iris.arff");
     }
 }
